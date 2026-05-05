790fc28ea65997addbf28eadace44e7971d69eaf
Michael Hoffert <mchoffert14@gmail.com>
2026-05-05 16:22:57 +0000

    update loc in rosetta xml: move rosetta scripts

--- scripts/run_rosetta.sh
+++ scripts/run_rosetta.sh
@@ -5,9 +5,11 @@ IND=$1
 touch ${IND}_inputs.txt
 echo "./test_structures2/${IND}_closed.pdb" >> ${IND}_inputs.txt
 
+# old loc: ~/tools/OGT_project/rosetta/ogt_metrics.xml
+
 ~/tools/rosetta.binary.linux.release-362/main/source/bin/rosetta_scripts.static.linuxgccrelease \
 -l ${IND}_inputs.txt \
--parser:protocol ~/tools/OGT_project/rosetta/ogt_metrics.xml \
+-parser:protocol /data/mhoffert/fiererlab/adenylate_kinase_ogt/data/rosetta/ogt_metrics.xml \
 -out:file:score_only ./rosetta_out/${IND}_rosetta_out.sc
 
 rm ${IND}_inputs.txt
